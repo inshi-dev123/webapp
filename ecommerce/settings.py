a68214111e2dfe9378fe4f09f1a43aba7df35ce9
Dev <dev@Devs-MacBook-Pro.local>
2025-06-30 09:57:25 -0700

    Add new Vercel domain to ALLOWED_HOSTS

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = ['ecommerce-peach-three.vercel.app', '127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['ecommerce-peach-three.vercel.app','ecom-phi-eosin.vercel.app' '127.0.0.1', 'localhost']
 
 
 # Application definition
